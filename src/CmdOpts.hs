66637dd8d9ae823ae91631215839402f8645b8c4
John Ky <newhoggy@gmail.com>
2023-08-28 00:23:10 +1000

    First commit

---
+++ src/CmdOpts.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE BlockArguments #-}
+
+module CmdOpts
+  ( Parser(..)
+  ) where
+
+import Control.Selective
+import Data.Bifunctor
+
+newtype Parser a = Parser
+  { unParser :: String -> Maybe (a, String)
+  }
+
+instance Functor Parser where
+  fmap f p =
+    Parser $ fmap (first f) . unParser p
+
+instance Applicative Parser where
+  pure a =
+    Parser $ \s -> pure (a, s)
+
+  pf <*> pa =
+    Parser \s -> do
+      (f, t) <- unParser pf s
+      (a, u) <- unParser pa t
+      pure (f a, u)
+
+instance Selective Parser where
+  select p q =
+    Parser \s -> do
+      (r, t) <- unParser p s
+      case r of
+        Left a -> do
+          (f, u) <- unParser q t
+          pure (f a, u)
+        Right b -> pure (b, t)
+
+instance Monad Parser where
+  pa >>= f =
+    Parser \s -> do
+      (a, t) <- unParser pa s
+      unParser (f a) t
+ 
